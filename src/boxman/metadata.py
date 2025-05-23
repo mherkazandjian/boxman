dd8107dc4c3a61342ab960a0446239808e27c28e
Mher Kazandjian <mherkazandjian@gmail.com>
2025-05-23 22:54:01 +0200

    bump the version to 0.8.0 dev

--- src/boxman/metadata.py
+++ src/boxman/metadata.py
@@ -1,7 +1,7 @@
 package = 'boxman'
 project = 'boxman'
 project_no_spaces = project.replace(' ', '')
-version = '0.7.0'
+version = '0.8.0dev'
 description = 'placeholder description for this project'
 authors = ['John Smith']
 authors_string = ', '.join(authors)
